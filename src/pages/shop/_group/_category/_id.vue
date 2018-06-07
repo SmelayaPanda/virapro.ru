f719133d18b40000d5ca056b79fb7a6e1649f4af
panda <SmelayaPanda@mail.ru>
2018-06-07 14:44:02 +0700

    load single product

--- src/pages/shop/_group/_category/_id.vue
+++ src/pages/shop/_group/_category/_id.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1> Router id: {{ $route.params.id }}</h1>
-    <p>{{ $store.getters.singleProduct }}</p>
-    <p>{{ $store.getters.singleProduct.SKU }}</p>
-    <p>{{ $store.getters.singleProduct.title }}</p>
-    <p>{{ $store.getters.singleProduct.description }}</p>
+    <!--<p>{{ $store.getters.singleProduct }}</p>-->
+    <p>SKU: {{ $store.getters.singleProduct.SKU }}</p>
+    <p>title: {{ $store.getters.singleProduct.title }}</p>
+    <p>description: {{ $store.getters.singleProduct.description }}</p>
+    <p>material: {{ $store.getters.singleProduct.material }}</p>
+    <p>color: {{ $store.getters.singleProduct.color }}</p>
+    <p>brand: {{ $store.getters.singleProduct.brand }}</p>
   </div>
 </template>
 
